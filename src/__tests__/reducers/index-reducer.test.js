d8730f9e3b61ca406a1004b8e4bf4b968ca24cef
Olha Hizhytska <olgainfotech@gmail.com>
2021-04-30 16:12:21 -0700

    add root reducer and corresponding tests

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,25 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegtListReducer from '../../reducers/keg-list-reducer';
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, { type: null })).toEqual({
+      masterKegList: {},
+      formVisibleOnPage: false
+    });
+  });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+  
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+});
