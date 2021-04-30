f29bfda118fc2808ae84e41a3e417d9d1bfba839
Olha Hizhytska <olgainfotech@gmail.com>
2021-04-30 16:49:39 -0700

    add addKeg amd toggleForm action crreators and corresponding tests

--- src/__tests__/actions/index.js
+++ src/__tests__/actions/index.js
@@ -1,7 +1,7 @@
 import * as actions from './../../actions';
 
 describe('help keg actions', () => {
-  
+
   it('deleteKeg should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
       type: 'DELETE_KEG',
@@ -9,5 +9,21 @@ describe('help keg actions', () => {
     });
   });
 
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+
+  it('addKeg should create ADD_KEG action', () => {
+    expect(actions.addKeg({name: 'Mezquita', brand: 'Alhabra', price: 5, abv:"4%", pintsLeft: 24, id: 1})).toEqual({
+      name: 'Mezquita',
+      brand: 'Alhabra',
+      price: 5,
+      abv: "4%",
+      pintsLeft: 24,
+      id: 1
+    });
+  });
 
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,20 @@ export const deleteKeg = id => ({
   type: 'DELETE_KEG',
   id
 });
+
+export const toggleForm = () => ({
+  type: 'TOGGLE_FORM'
+});
+
+export const addKeg = (keg) => {
+  const { name, brand, price, abv, pintsLeft, id } = keg;
+  return {
+    type: 'ADD_KEG',
+    name: name,
+    brand: brand,
+    price: price,
+    abv: abv,
+    pintsLeft: pintsLeft,
+    id: id
+  }
+}
