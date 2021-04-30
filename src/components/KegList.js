62215763d84e28eca3bd41ab83538a7c7304d99a
Olha Hizhytska <olgainfotech@gmail.com>
2021-04-30 12:41:34 -0700

    add keg list reducer and test

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -7,26 +7,25 @@ function KegList(props){
     <React.Fragment>
       <hr/>
       {props.kegList.map((keg) =>
-          <Keg 
-            whenKegClicked = {props.onKegSelection}
-            name={keg.name}
-            brand={keg.brand}
-            price={keg.price}
-            abv={keg.abv}
-            pintsLeft={keg.pintsLeft}
-            id={keg.id}
-            key={keg.id}
-            whenSellClicked={props.onDecrease}/>
-    )}
-    </React.Fragment>
-    
-  );
+        <Keg 
+          whenKegClicked = {props.onKegSelection}
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          abv={keg.abv}
+          pintsLeft={keg.pintsLeft}
+          id={keg.id}
+          key={keg.id}
+          whenSellClicked={props.onDecrease}/>
+      )}
+    </React.Fragment>  
+  ); 
 }
 
 KegList.propTypes = {
   kegList: PropTypes.array,
   onKegSelection: PropTypes.func,
   onDecrease: PropTypes.func  
-}
+};
 
 export default KegList;
