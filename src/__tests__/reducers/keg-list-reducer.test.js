fffeb1a13e0229620909d56d0a13d8dcae3b81c6
Olha Hizhytska <olgainfotech@gmail.com>
2021-04-30 13:43:14 -0700

    add delete logic to list reducer and test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -12,6 +12,21 @@ describe('kegListReducer', () => {
     id: 1
   };
 
+  const currentState = {
+    1: {name: 'Mezquita',
+      brand: 'Alhabra',
+      price: 5,
+      abv: "4%",
+      pintsLeft: 24,
+      id: 1 },
+    2: {name: 'Old Fashion',
+      brand: 'Alhabra',
+      price: 4,
+      abv: "4%",
+      pintsLeft: 24,
+      id: 2}
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
 
     expect(kegListReducer({}, { type: null })).toEqual({});
@@ -38,6 +53,21 @@ describe('kegListReducer', () => {
         pintsLeft: pintsLeft,
         id: id
       }
+    });
+  });
+
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: {name: 'Old Fashion',
+        brand: 'Alhabra',
+        price: 4,
+        abv: "4%",
+        pintsLeft: 24,
+        id: 2}
     });
   });
 
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -12,6 +12,10 @@ export default (state = {}, action) => {
         id: id
       }
     });
+  case 'DELETE_KEG':
+    let newState = { ...state };
+    delete newState[id];
+    return newState;
   default:
     return state;
   }
